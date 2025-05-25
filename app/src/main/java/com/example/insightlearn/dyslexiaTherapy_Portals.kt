7d414844ccecbf1a5d74112e38c84c4828f5e550
Raveeha Choudhery <raveehachoudhry@gmail.com>
2025-05-25 05:02:50 +0500

    Initial commit

--- app/src/main/java/com/example/insightlearn/dyslexiaTherapy_Portals.kt
+++ app/src/main/java/com/example/insightlearn/dyslexiaTherapy_Portals.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import seasonstart
+
 
 class DyslexiaTherapy_Portals : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
